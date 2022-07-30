087c6c0a07ec4ecb4d0547225f48d3eda246e964
Jairo Ferreira <jf.jairo@gmail.com>
2022-07-30 16:10:18 -0400

    Verificando se é String ou nao

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+const nome = new Boolean('jairo');
+const email = new Boolean('jf.jairo@gmail.com');
+const profissao = new Boolean('Consultor de T.I');
+const idade = new Boolean();
+const viagemDosSonhos = new Boolean('Philippines');
+
+console.log(nome.valueOf());
+console.log(email.valueOf());
+console.log(profissao.valueOf());
+console.log(idade.valueOf());
+console.log(viagemDosSonhos.valueOf());
