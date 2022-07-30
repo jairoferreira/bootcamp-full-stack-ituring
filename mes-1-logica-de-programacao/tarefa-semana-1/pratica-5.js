68cd16119bf11f04325d4b1058d2f4e019595180
Jairo Ferreira <jf.jairo@gmail.com>
2022-07-30 17:58:22 -0400

    Pratica 5: Preparando uma mensagem de WhatsApp com JavaScritp

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,9 @@
+let nome = "Jairo"
+let email = "jf.jairo@gmail.com"
+let profissao = "Consultor de T.I"
+let idade = 39
+let viagemDosSonhos = "Philippines"
+
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + " .Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email " + email + " ? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem);
