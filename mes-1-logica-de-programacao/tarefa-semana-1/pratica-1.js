45488fe5d4dc902f5edf5ff217d76d208a958c26
Jairo Ferreira <jf.jairo@gmail.com>
2022-07-29 20:22:04 -0400

    exibindo cada valores e tipos de cada uma das variáveis

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -4,4 +4,14 @@ let profissao = "Consultor de T.I"
 let idade = 39
 let viagemDosSonhos = "Philippines"
 
-console.log(viagemDosSonhos)
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(idade);
+console.log(viagemDosSonhos);
+
+console.log(typeof nome);
+console.log(typeof email);
+console.log(typeof profissao);
+console.log(typeof idade);
+console.log(typeof viagemDosSonhos);
