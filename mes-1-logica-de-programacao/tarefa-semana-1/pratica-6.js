07f6b4e32b4608a2582337c6c43ec6e3131ff200
Jairo Ferreira <jf.jairo@gmail.com>
2022-07-30 20:35:13 -0400

    Prática 6: Exibindo a tabuada de 123

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,32 @@
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+
+let resultado = numero1*=123
+let resultado2 = numero2*=123;
+let resultado3 = numero3*=123;
+let resultado4 = numero4*=123;
+let resultado5 = numero5*=123;
+let resultado6 = numero6*=123;
+let resultado7 = numero7*=123;
+let resultado8 = numero8*=123;
+let resultado9 = numero9*=123;
+let resultado10 = numero10*123;
+
+console.log(resultado);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+console.log(resultado6);
+console.log(resultado7);
+console.log(resultado8);
+console.log(resultado9);
+console.log(resultado10);  
