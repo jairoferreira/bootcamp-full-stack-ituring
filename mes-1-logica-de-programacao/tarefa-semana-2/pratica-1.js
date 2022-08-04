2de6180c1fe81fb08a5ae63402dbf552e7c86269
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-04 14:44:20 -0400

    Prática 1: Implementando minha 1ª função

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-1.js
@@ -1,6 +1,9 @@
+/* Declare uma função com o nome de helloWorld, que não receberá nenhum argumento
+de entrada e que exiba na tela o seguinte conteúdo: Hello, World! :)*/
+
 function helloWolrd() {
 
-    console.log("Hello, Wolrd! :)")
+    console.log("Hello, Wolrd! ;-)")
 }
 
 helloWolrd()
---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -0,0 +1,12 @@
+/*Fizemos a entrega do produto NomeDoProduto no endereço EnderecoDeEntrega 
+com sucesso!
+O seu produto foi recebido por: NomeDaPessoaQueRecebeuOProduto Esperamos que você 
+esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que 
+possamos ajudar.*/
+
+function digaOla(nome) {
+
+    console.log("Olá ," + nome)
+}
+digaOla("Jairo Ferreira")
+
