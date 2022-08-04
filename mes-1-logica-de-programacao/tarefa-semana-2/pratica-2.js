d517ca17491619c6696155e41312c457632bed6a
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-04 16:16:55 -0400

    Prática 2: Funções com parâmetros

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -8,5 +8,16 @@ function digaOla(nome) {
 
     console.log("Olá ," + nome)
 }
-digaOla("Jairo Ferreira")
+
+const nomeDoProduto = "Fone de Ouvido"
+let EnderecoDeEntrega = "Av. Eduardo Ribeiro nº 20 - Centro - Manaus-AM"
+let NomeDaPessoaQueRecebeuOProduto = "Maria Expedita"
+
+digaOla("Jairo Ferreira");
+
+console.log(`Fizemos a entrega do produto ${nomeDoProduto}, no endereço ${EnderecoDeEntrega} 
+com sucesso!`)
+console.log(`O seu produto foi recebido por: ${NomeDaPessoaQueRecebeuOProduto} Esperamos que você 
+esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que 
+possamos ajudar.`)
 
