85084cc56e051d3e80db738c716405b047666c74
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-04 16:55:23 -0400

    Etapa 2: Dominando a criação de funções - Prática 2: Funções com parâmetros

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -9,14 +9,17 @@ function digaOla(nome) {
     console.log("Olá ," + nome)
 
 }
-const nomeDoProduto = "Fone de Ouvido"
+let NomeDoProduto = "Fone de Ouvido"
 let EnderecoDeEntrega = "Av. Eduardo Ribeiro nº 20 - Centro - Manaus-AM"
 let NomeDaPessoaQueRecebeuOProduto = "Maria Expedita"
+let NomeDaPessoaQueEnviouOEmail = "amazon@amazon.com"
 
 digaOla("Jairo Ferreira");
 
-console.log(`Fizemos a entrega do produto ${nomeDoProduto}, no endereço ${EnderecoDeEntrega} 
+console.log(`Fizemos a entrega do produto ${NomeDoProduto}, no endereço ${EnderecoDeEntrega} 
 com sucesso!`)
 console.log(`O seu produto foi recebido por: ${NomeDaPessoaQueRecebeuOProduto} Esperamos que você 
 esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que 
 possamos ajudar.`)
+
+console.log("Atenciosamente, " + NomeDaPessoaQueEnviouOEmail);
