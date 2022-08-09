adee95ce88297e1aaddde719bed9dd9c40ed9c4c
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-08 20:03:37 -0400

    Corrigindo exercicio - Prática 2: Funções com parâmetros

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -1,3 +1,26 @@
-function saudacao(nome) {
-    console.log(`Olá, ${nome}`)
+let nomeDoProduto ="Phone de Ouvido"
+let enderecoDestinatario = "Av. Eduardo Ribeiro"
+let recebedorDoProduto = "Maria Fernanda"
+let nomedoRemetente = "Antonio José"
+
+function digaOla(nome) {
+    
+    console.log(`Olá,` + nome)
 }
+
+function corpo(nomeDoProduto, recebedorDoProduto, enderecoDestinatario) {
+    const mensagem = `Fizemos a entrega do produto ${nomeDoProduto} no endereço ${enderecoDestinatario} com sucesso!
+
+O seu produto foi redebido por: ${recebedorDoProduto}
+
+Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.`
+    console.log(mensagem)
+}
+
+function agradecimento(nomedoRemetente) {
+       console.log(`Atenciosamente, ${nomedoRemetente}`)
+}
+
+digaOla("Jairo")
+corpo(nomeDoProduto, recebedorDoProduto, enderecoDestinatario)
+agradecimento(nomedoRemetente)
