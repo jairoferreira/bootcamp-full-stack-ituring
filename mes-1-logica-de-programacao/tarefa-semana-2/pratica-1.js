dd27be55a69f36b1eb92a267b8a8f866a5abac6e
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-10 05:39:32 -0400

    Prática 1: Implementando minha 1ª função - Review 01

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-1.js
@@ -1,9 +1,9 @@
 /* Declare uma função com o nome de helloWorld, que não receberá nenhum argumento
 de entrada e que exiba na tela o seguinte conteúdo: Hello, World! :)*/
 
-function helloWolrd() {
+function helloWorld() {
 
-    console.log("Hello, Wolrd! ;-)")
+    console.log("Hello, World! ;-)")
 }
 
-helloWolrd()
+helloWorld()
