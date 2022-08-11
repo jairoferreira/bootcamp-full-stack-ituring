4032ba80ad9599d688e177e981175f2f3b11e163
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-10 20:05:27 -0400

    Prática 7: Tradutor de Hello World - Em 4 idiomas

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,23 @@
+function helloWorld3idiomas(idioma) {
+    if (idioma == "en") {
+        console.log('Hello World!!! ');
+
+    } else if (idioma == "es") {
+        console.log('Hola mundo!!!');
+
+    } else if (idioma == "it") {
+        console.log('Ciao mondo!!!')
+    }
+    else
+
+    console.log('Olá mundo!!!')
+}
+
+helloWorld3idiomas("")
+
+// "en" retorna:    Hello World!!!
+// "es" retorna:    Hola mundo!!!
+// "it" retorna:    Ciao mondo!!!
+// "  " retorna:    Olá mundo!!!
+
+
