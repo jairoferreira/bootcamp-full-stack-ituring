57d6fa49153b4c170b299f146418a9d4eb998881
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-11 21:21:32 -0400

    Prática 9: Qual é o maior de 2 números?

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -0,0 +1,11 @@
+function maiorNumeroDe2(n1, n2) {
+    if (n1 > n2) {
+        console.log(n1)
+    } else {
+        console.log(n2)
+    }
+}
+
+maiorNumeroDe2(-0,-2)
+maiorNumeroDe2(1, 5)
+maiorNumeroDe2(10, 7)
