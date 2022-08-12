b2de2cbfa3e4ed8469776ef80705f12a67f800b4
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-11 21:29:41 -0400

    Prática 10: Qual é o maior de 3 números?

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -0,0 +1,13 @@
+function maiorNumeroDe3(n1, n2, n3) {
+    if (n1 > n2 && n1 > n3){
+        console.log(n1)
+} else if (n2 > n3) {
+    console.log(n2)
+} else {
+    console.log(n3)
+}
+}
+
+maiorNumeroDe3(33,22,11)
+maiorNumeroDe3(30,90,60)
+maiorNumeroDe3(-10,-100,-1000)
