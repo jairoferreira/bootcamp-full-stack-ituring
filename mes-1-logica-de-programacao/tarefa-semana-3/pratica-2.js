8cb1e88edb01958733ffd75fce049310fe9ebc4a
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-13 20:10:21 -0400

    Prática 2: Exiba os ímpares - Review - 1

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
@@ -1,9 +1,12 @@
-let n = 1, impares = "";
-while (n <= 17) {
 
-    impares += !(n % 2 == 0) ? n + "," : "";
-    n++;
-};
+function Impar(numero) {
+    for (let i = 0; i <= numero; i++) {
+      if (i % 2 !== 0) {
+        console.log(i);
+      }
+    }
+  
+  }
+  console.log(`Dado de entrada da função: ${"17"}`)
 
-console.log(`Dado de entrada da função: ${"17"}`)
-console.log(impares);
+  Impar(17)
