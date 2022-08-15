057e3c98df6d2c592f309c588f120b58a6627dd7
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-15 17:18:07 -0400

    Prática 5: Procurando um seriado especial

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -0,0 +1,8 @@
+function listFilmes() {
+    let lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+    console.log(lista.lastIndexOf("Dexter"));
+    console.log(("Dexter"));
+   
+}
+
+listFilmes(["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"])
