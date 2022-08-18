5251b7ac8d3b1b995d3279a21bf77c97e8b3a25b
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-17 22:05:42 -0400

    Late task - Prática 7: Calculando o saldo final - Revisando Assunto

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
@@ -0,0 +1,17 @@
+/*
+Crie uma função no arquivo pratica-7.js que recebendo como argumento uma
+lista de depósitos e retiradas como no exemplo abaixo, exiba o saldo final da conta.
+
+Dado de entrada da função:
+
+*/
+
+
+function calculaSaldoFinal(){
+    let depositos = [100, -20, -30, 10, -7, -21, -5]
+    console.log(depositos)
+    console.log("O saldo final de sua conta foi positivo no valor de R$ 27.")
+
+}
+
+calculaSaldoFinal()
