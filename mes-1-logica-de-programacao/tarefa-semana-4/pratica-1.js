68a6f6661759630288f18ad6601490b9fde550ba
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-19 23:01:34 -0400

    Prática 1: Utilizando o método .forEach() (parte 1) - Review 01

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-1.js
@@ -0,0 +1,4 @@
+let listaSeriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+listaSeriados.forEach(function (name, index) {
+    console.log(index, name);
+})
