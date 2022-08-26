bc67d33b595c9ebfba4e9325eea95027081a8ffc
Jairo Ferreira <jf.jairo@gmail.com>
2022-08-26 16:53:33 -0400

    Prática 2: Utilizando o método .forEach() (parte 2)

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-2.js
@@ -1,5 +1,23 @@
 
-let listaSeriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
-listaSeriados.forEach(function (name, index) {
-    console.log(index, name);
-})
+/*
+const seriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
+
+const index = seriados.findIndex((seriados) => seriados === "Dexter");
+
+console.log(index);
+console.log(seriados[index]);
+*/
+
+function encontrar(array, nome) {
+  let indiceEncontrado = undefined
+
+  array.forEach(function (item, index) {
+    if (item === nome) {
+      indiceEncontrado = index
+    }
+  })
+ // return indiceEncontrado
+}
+
+console.log(encontrar["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"],
+"Dexter")
